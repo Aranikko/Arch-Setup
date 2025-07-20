e6d96cf1586c0487e86ee30b77ea71df78f02de4
Aranikko <129112827+Aranikko@users.noreply.github.com>
2025-07-20 14:36:24 +0300

    .

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+git_clone(){
+
+	git clone https://aur.archlinux.org/yay.git
+	git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
+	git clone https://github.com/itRoy-pentest/RoyHyprland.git
+
+}
+
 install_dependencies(){
 
 	sudo pacman -S freetype2 zsh hyprlock hyprpaper waybar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono pavucontrol feh ranger thunar meson nwg-look papirus-icon-theme fastfetch file powerline-fonts inetutils ttf-font-awesome otf-font-awesome ttf-jetbrains-mono  ttf-dejavu bluez bluez-utils blueman vlc tar unzip zip 7zip
@@ -26,5 +34,4 @@ install_zsh(){
 
 }
 
-install_zsh
-
+git_clone
